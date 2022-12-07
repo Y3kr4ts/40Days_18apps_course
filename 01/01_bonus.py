087725fcf4b56706e6c911a6f07ee8ce7dd8fed0
Y3krats laehc1m <y3krats@outlook.com>
2022-12-07 02:00:01 +0000

    day 1

---
+++ 01/01_bonus.py
@@ -0,0 +1,3 @@
+text = input("Say a word: ")
+length = len(text)
+print("The length of the title: ", length)
---
+++ 01/01_exercises.py
@@ -0,0 +1,10 @@
+# 1
+my_var = "String Stored In Variable"
+print(type(my_var))
+
+# 2
+greeting = input("Hello what is your name? ")
+print("Hello", greeting)
+
+# 3
+my_list = ['boy', 'girl', 'cat', 'dog']
